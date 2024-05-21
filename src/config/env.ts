41fb3c97334c10860fc5b96c8b29ff08a957a711
gabrielalmir <gabr.almir@gmail.com>
2024-05-21 20:32:24 -0300

    feat: config basic database setup

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,9 +1,12 @@
-import 'dotenv/config'
-import { z } from 'zod'
+import 'dotenv/config';
+import { z } from 'zod';
 
-const envSchema = z.object({
+export const envSchema = z.object({
     PORT: z.string().default('3000'),
     NODE_ENV: z.string().default('development'),
-})
+    DATABASE_URL: z.string(),
+    AUTHORIZATION_SERVICE_URL: z.string().url().default('https://run.mocky.io/v3/5794d450-d2e2-4412-8131-73d0293ac1cc'),
+    NOTIFICATION_SERVICE_URL: z.string().url().default('https://run.mocky.io/v3/54dc2cf1-3add-45b5-b5a9-6bf7e7f1f4a6'),
+});
 
-export const env = envSchema.parse(process.env)
+export const env = envSchema.parse(process.env);
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
+import { envSchema } from './config/env';
 
 async function bootstrap() {
+  // Setup the environment variables
+  envSchema.parse(process.env);
+
+  // Create the NestJS application
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter()
   );
+
+  // Start the application
   await app.listen(3000);
 }
 

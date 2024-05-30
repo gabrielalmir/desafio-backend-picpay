1b409743e493192c02e24840ab2dde91cba39dc5
gabrielalmir <gabr.almir@gmail.com>
2024-05-30 13:03:46 -0300

    feat: remove unneeded parse in bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
-import { envSchema } from './config/env';
 
 async function bootstrap() {
-  // Setup the environment variables
-  envSchema.parse(process.env);
-
   // Create the NestJS application
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
